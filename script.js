b9ffbec9d4fe67f90976095820a02c1cfea30287
RiyadTangil <riyadkhanxp2@gmail.com>
2021-02-07 23:58:55 +0600

    last output

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-//##############getting HTML element #######################
+
 const mealInput = document.getElementById('display-text');
 
-function getMeal(){
+function getMeal() {
     const mealInputText = mealInput.value;
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${mealInputText}`)
         .then(res => res.json())
         .then(data => displayMeal(data))
-        
-       
 
-        const displayMeal = meal =>{
-             mealsDiv = document.getElementById('meal-div');
-            meal.meals.forEach(mealList => { 
-                console.log(mealList);
-                
-                const mealDiv= document.createElement('div');
-                mealDiv.className = "mealClass";
-                 
-                const mealInfo = `
+    const displayMeal = meal => {
+        mealsDiv = document.getElementById('meal-div');
+        meal.meals.forEach(mealList => {
+            console.log(mealList);
+
+            const mealDiv = document.createElement('div');
+            mealDiv.className = "mealClass";
+
+            const mealInfo = `
                <img onclick="mealDetails('${mealList.strMeal}')" src = "${mealList.strMealThumb}">
                 <h3>${mealList.strMeal}</h3>`
-                mealDiv.innerHTML = mealInfo;
-                mealsDiv.appendChild(mealDiv);
+            mealDiv.innerHTML = mealInfo;
+            mealsDiv.appendChild(mealDiv);
 
-               
-                
-            });
-            
-        }
-        
-        
+
+
+        });
+
+    }
+
+
 }
-const  mealDetails =name =>{
-     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
-     fetch(url)
-     .then(res => res.json())
-     .then(data => renderMealInfo(data.meals))
+const mealDetails = name => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => renderMealInfo(data.meals))
 }
 
-const renderMealInfo = meal =>{
-const mealInfo= document.getElementById("meal-details");
-console.log(meal);
+const renderMealInfo = meal => {
+    const mealInfo = document.getElementById("meal-details");
+    console.log(meal);
     meal = meal[0];
-   
+
     let html = `
         <h2 class = "recipe-title">${meal.strMeal}</h2>
         <p class = "recipe-category">${meal.strCategory}</p>
@@ -58,7 +56,7 @@ console.log(meal);
         </div>
     `;
 
-    mealInfo.innerHTML= html;
+    mealInfo.innerHTML = html;
 }
 
 
