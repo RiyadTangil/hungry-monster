f9ddfa83c377fcda41fb461e1f042d91aaecdb29
RiyadTangil <riyadkhanxp2@gmail.com>
2021-02-08 21:28:49 +0600

    last commit

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 
 const mealInput = document.getElementById('search-text');
+
 //###################get search result of meal#########################
 function getMeal() {
+    let mealDetails = document.getElementById("meal-details");
+    let mealDiv = document.getElementById("meal-div");
     const mealInputText = mealInput.value;
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${mealInputText}`)
         .then(res => res.json())
         .then(data => displayMeals(data))
 
-    const displayMeals = meal => {
 
-        mealsDiv = document.getElementById('meal-div');
-        meal.meals.forEach(mealList => {    
-            const mealDiv = document.createElement('div');
-            mealDiv.className = "meal-container";
-            const mealInfo = `
+    const displayMeals = meal => {
+        if (meal.meals) {
+            mealDetails.style.display="none"
+
+            mealsDiv = document.getElementById('meal-div');
+            meal.meals.forEach(mealList => {
+                const mealDiv = document.createElement('div');
+                mealDiv.className = "meal-container";
+                const mealInfo = `
                <img onclick="mealDetails('${mealList.idMeal}')" src = "${mealList.strMealThumb}">
                 <h3 onclick="mealDetails('${mealList.idMeal}')">${mealList.strMeal}</h3>`
-            mealDiv.innerHTML = mealInfo;
-            mealsDiv.appendChild(mealDiv);
-        });
+                mealDiv.innerHTML = mealInfo;
+                mealsDiv.appendChild(mealDiv);
+            });
+        }
+        else {
+           
+            let html = `<h1>No meal found!</h1>`
+            mealDetails.innerHTML = html;
+            mealDetails.style.display = "block"
+            mealDiv.style.display="none"
+
+
+        }
+
+
     }
 }
 
@@ -33,22 +51,21 @@ const mealDetails = id => {
 
 const renderMealInfo = meal => {
     const mealDetails = document.getElementById("meal-details");
-    console.log(meal);
     meal = meal[0];
 
     let html = `
     <button onclick="displayNone()">x</button>
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <h2>${meal.strMeal}</h2>
         
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
+        <p>${meal.strCategory}</p>
+        <div>
             <h3>Instructions:</h3>
             <p>${meal.strInstructions}</p>
         </div>
-        <div class = "recipe-meal-img">
+        <div>
             <img src = "${meal.strMealThumb}" alt = "">
         </div>
-        <div class = "recipe-link">
+        <div>
             <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
         </div>
     `;
